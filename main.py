120969c2743176591dc30d8702829da16df93986
alex <alexemfan@mail.ru>
2015-10-18 16:48:39 +0300

    add bg, markup

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from kivy.app import App
 from initial import *
+from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
+
+Window.clearcolor = get_color_from_hex('#101215')
 
 
 class ClockApp(App):
